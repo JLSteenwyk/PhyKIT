04dfa00ea01dfae562e447b961ea177315d5a409
Thomas Buida <thomasbuida@gmail.com>
2020-06-21 11:39:20 -0500

    Add more alignment integration tests

--- tests/integration/test_alignment.py
+++ tests/integration/test_alignment.py
@@ -2,18 +2,70 @@ import pytest
 import sys
 from mock import patch, call
 from pathlib import Path
+from textwrap import dedent
 
 from phykit.phykit import Phykit
 
 here = Path(__file__)
 
+
 @pytest.mark.integration
 class TestAlignment(object):
+    @patch("builtins.print")
+    def test_alignment_length(self, mocked_print):
+        expected_result = "6"
+        testargs = [
+            "phykit",
+            "alignment_length",
+            f"{here.parent.parent}/sample_files/simple.fa",
+        ]
+        with patch.object(sys, "argv", testargs):
+            Phykit()
+        assert mocked_print.mock_calls == [call(expected_result)]
 
-	@patch('builtins.print')
-	def test_alignment_length(self, mocked_print):
-		expected_length = '6'
-		testargs = ["clipkit", "alignment_length", f"{here.parent.parent}/sample_files/simple.fa"]
-		with patch.object(sys, 'argv', testargs):
-			Phykit()
-		assert mocked_print.mock_calls == [call(expected_length)]
+    @patch("builtins.print")
+    def test_alignment_length_no_gaps(self, mocked_print):
+        expected_result = "3\t6\t50.0"
+        testargs = [
+            "phykit",
+            "alignment_length_no_gaps",
+            f"{here.parent.parent}/sample_files/simple.fa",
+        ]
+        with patch.object(sys, "argv", testargs):
+            Phykit()
+        assert mocked_print.mock_calls == [call(expected_result)]
+
+    @patch("builtins.print")
+    def test_create_concatenation_matrix(self, mocked_print):
+        expected_start_message = dedent(
+            """
+			--------------------
+			| General features |
+			--------------------
+			Total number of taxa: 12
+			Total number of alignments: 3
+			
+			
+			----------------
+			| Output files |
+			----------------
+			partition file output: output/create_concat_matrix.partition
+			concatenated fasta output: output/create_concat_matrix.fa
+			occupancy report: output/create_concat_matrix.occupancy
+			"""
+        )
+        testargs = [
+            "phykit",
+            "create_concatenation_matrix",
+            "-a",
+            f"{here.parent.parent}/sample_files/alignment_list_for_create_concat_matrix.txt",
+            "-p",
+            "output/create_concat_matrix",
+        ]
+        with patch.object(sys, "argv", testargs):
+            Phykit()
+        assert mocked_print.mock_calls == [
+            call(expected_start_message),
+            call("Complete!\n"),
+        ]
+
