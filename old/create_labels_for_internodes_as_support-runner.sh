443e4d46141685aeb1037c7eb7f3cdbd4252dc57
Thomas Buida <thomasbuida@gmail.com>
2020-06-21 16:09:15 -0500

    Remove old files

--- old/create_labels_for_internodes_as_support-runner.sh
+++
@@ -1,1 +0,0 @@
-python3 create_labels_for_internodes_as_support.py -t tree_simple.tre
--- old/threadNuclProt-runner.sh
+++
@@ -1,1 +0,0 @@
-python3 threadNuclProt.py -p EOG091N44MS.fa.mafft -n EOG091N44MS.fa -s F |tee EOG091N44MS.fa.mafft.threaded
