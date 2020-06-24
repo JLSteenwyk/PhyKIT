c118f878d76cdab8de49bb05efc6f5e45c7251c7
Jacob Steenwyk <jlsteenwyk@gmail.com>
2020-06-24 13:35:19 -0700

    refactored create_concatentation_matrix and included type hints throughout

--- phykit/services/alignment/create_concatenation_matrix.py
+++ phykit/services/alignment/create_concatenation_matrix.py
@@ -1,7 +1,9 @@
 import statistics as stat
+from textwrap import dedent
+from typing import Tuple
+
 from Bio import SeqIO, SeqRecord
 from Bio.Seq import Seq
-from textwrap import dedent
 
 from .base import Alignment
 
@@ -16,16 +18,16 @@ class CreateConcatenationMatrix(Alignment):
         
         self.create_concatenation_matrix(alignment_list, prefix)
 
-    def process_args(self, args):
+    def process_args(self, args) -> dict:
         return dict(
             alignment_list=args.alignment_list,
             prefix=args.prefix
         )
 
-    def create_concatenation_matrix(self, alignment_list, prefix):
-        # read lists into python lists
-        alignments  = [line.rstrip('\n') for line in open(alignment_list)]
-
+    def get_taxa_names(
+        self,
+        alignments: list
+    ) -> list:
         # get taxa names
         taxa = []
         for alignment in alignments:
@@ -33,13 +35,21 @@ class CreateConcatenationMatrix(Alignment):
             for indiv in alignment:
                 if indiv.name not in taxa:
                     taxa.append(indiv.name)
+        return taxa
+    
+    def start_message_print(
+        self,
+        taxa: list,
+        alignments: list,
+        file_partition: str,
+        fasta_output: str,
+        file_occupancy: str
+    ) -> None:
+        """
+        print a log message to the user about general features of
+        input and output file names
+        """
 
-        # assignment the output file names
-        file_partition = prefix + ".partition"
-        fasta_output   = prefix + ".fa"
-        file_occupancy = prefix + ".occupancy"
-
-        # print log message
         start_message = dedent(f"""
             --------------------
             | General features |
@@ -55,9 +65,173 @@ class CreateConcatenationMatrix(Alignment):
             Concatenated fasta output: {fasta_output}
             Occupancy report: {file_occupancy}
         """)
+
         print(start_message)
 
-        # # assigning placeholders for lengths
+    def fasta_file_write(
+        self,
+        fasta_output: str,
+        concat: dict
+    ) -> None:
+        """
+        join seqs of genes in value (list) and write to concat_fa 
+        """
+
+        with open(fasta_output, "w") as final_fasta_file: 
+            for x in concat:
+                concatenated = Seq("")
+                for s in concat[x]:
+                    concatenated += s
+                concat[x] = concatenated
+                entry = '>'+x+"\n"+concat[x]+"\n"
+                final_fasta_file.write(str(entry))     
+
+    def occupancy_file_write(
+        self,
+        file_occupancy: str,
+        og_taxa: list,
+        missing_taxa: list,
+        fasta: str
+    ) -> None:
+        """
+        write partition file to detail boundaries in the concat alignment
+        """
+
+        with open(file_occupancy, "a") as f:
+            # determine number of present taxa
+            num_present  = len(og_taxa)
+            # determine number of missing taxa
+            num_missing  = len(missing_taxa)
+            # determine percent occupied
+            percent_occupancy = num_present/(num_present+num_missing)
+            if num_missing == 0:
+                missing_taxa = ["None"]
+            entry = str(fasta)+'\t'+str(num_present)+'\t'+str(num_missing)+'\t'+str(percent_occupancy)+'\t'+';'.join(missing_taxa)+'\t'+';'.join(og_taxa)+"\n"
+            f.write(str(entry))
+
+    def partition_file_write(
+        self,
+        file_partition: str,
+        og_len: int,
+        field_one: str,
+        fasta: str,
+        first_len: int,
+        second_len: int
+    ) -> Tuple[int, int]:
+        """
+        append to partition file
+        """
+        with open(file_partition, "a") as f:
+            # second value in partition file
+            second_len += og_len
+            entry = field_one+", "+str(fasta)+"="+str(first_len)+"-"+str(second_len)+"\n"
+            f.write(str(entry))
+            # add to first value for partition file
+            first_len += og_len
+
+        return first_len, second_len
+
+    def create_missing_taxa_sequence(
+        self,
+        missing_taxa: list,
+        missing_seq: str,
+        concat: dict
+    ) -> dict:
+
+        for indiv in missing_taxa:
+            indiv_record = SeqRecord(Seq(missing_seq), 
+                id = indiv, name = indiv, description = indiv)
+            concat[indiv].append(indiv_record.seq) 
+
+        return concat 
+
+    def create_record_for_present_taxa(
+        self,
+        records: list,
+        og_taxa: list,
+        concat: dict
+    ) -> dict:
+        """
+        create a record for taxa that are present in the concatenation dict
+        """
+        for record in records:
+            if record.id in og_taxa:
+                concat[record.id].append(record.seq)
+
+        return concat
+
+    def create_missing_seq_str(
+        self,
+        records: list
+    ) -> Tuple[str, int]:
+        """
+        create a placeholder string for sequences with missing taxa
+        """
+
+        og_len     = ''
+        missing_seq = ''
+        og_len   = len(records[0].seq)
+        missing_seq = og_len*'?'
+    
+        return missing_seq, og_len
+
+    def determine_missing_sequences(
+        self,
+        taxa: list,
+        og_taxa: list
+    ) -> list:
+        """
+        determine taxa not present in an alignment file
+        """
+
+        missing_taxa = list(set(taxa)-set(og_taxa))
+
+        return missing_taxa
+
+    def get_list_of_taxa_and_records(
+        self,
+        fasta: str
+    ) -> Tuple[list, list]:
+        """
+        get list of a taxa and their records
+        """
+        # list for keeping track of taxa per USCO
+        og_taxa    = []
+        # open alignment file
+        records = list(SeqIO.parse(fasta, "fasta"))
+
+        # create lists of USCOtaxa and missing_taxa
+        for record in records:
+            # append taxa per USCO to USCOtaxa
+            og_taxa.append(record.id)
+
+        return og_taxa, records
+
+    def create_concatenation_matrix(
+        self,
+        alignment_list: str,
+        prefix: str
+    ):
+        # read lists into python lists
+        alignments  = [line.rstrip('\n') for line in open(alignment_list)]
+
+        taxa = self.get_taxa_names(alignments)
+
+        # assignment the output file names
+        file_partition = prefix + ".partition"
+        fasta_output   = prefix + ".fa"
+        file_occupancy = prefix + ".occupancy"
+
+        # print log message
+        self.start_message_print(
+            taxa,
+            alignments,
+            file_partition,
+            fasta_output,
+            file_occupancy
+        )
+
+        # assign placeholders for lengths in the partition file
         first_len    = 1
         second_len   = 0
 
@@ -71,70 +245,63 @@ class CreateConcatenationMatrix(Alignment):
         open(file_partition, "w")
         # create an empty occupancy per OG file
         open(file_occupancy, "w")
-        # loop through USCO trimal files
-        for fasta in alignments:
-            # list for keeping track of taxa per USCO
-            og_taxa    = []
-            # open alignment file
-            records = list(SeqIO.parse(fasta, "fasta"))
 
-            # create lists of USCOtaxa and missing_taxa
-            for record in records:
-                # append taxa per USCO to USCOtaxa
-                og_taxa.append(record.id)
+        # loop through USCOs files
+        for fasta in alignments:
+
+            # create a list of taxa and records
+            og_taxa, records = self.get_list_of_taxa_and_records(
+                fasta
+            )
                 
             # initialize list and determine missing taxa
-            missing_taxa = []
-            missing_taxa = list(set(taxa)-set(og_taxa))
+            missing_taxa = self.determine_missing_sequences(
+                taxa,
+                og_taxa
+            )
 
             # create string for missing seq
-            og_len     = ''
-            missing_seq = ''
-            og_len   = len(records[0].seq)
-            missing_seq = og_len*'?'
+            missing_seq, og_len = self.create_missing_seq_str(
+                records
+            )
 
-            # create missing taxa sequence based on either it is protein or nucleotide
-            for indiv in missing_taxa:
-                indiv_record = SeqRecord(Seq(missing_seq), 
-                    id = indiv, name = indiv, description = indiv)
-                concat[indiv].append(indiv_record.seq)        
+            # create missing taxa sequence
+            concat = self.create_missing_taxa_sequence(
+                missing_taxa,
+                missing_seq,
+                concat
+            )      
 
             # create record for present data
-            for record in records:
-                if record.id in og_taxa:
-                    concat[record.id].append(record.seq)
+            concat = self.create_record_for_present_taxa(
+                records,
+                og_taxa,
+                concat
+            )
 
-            # create partition file
-            with open(file_partition, "a") as f:
-                # second value in partition file
-                second_len += og_len
-                entry = field_one+", "+str(fasta)+"="+str(first_len)+"-"+str(second_len)+"\n"
-                f.write(str(entry))
-                # add to first value for partition file
-                first_len += og_len
+            # append to partition file
+            first_len, second_len = self.partition_file_write(
+                file_partition,
+                og_len,
+                field_one,
+                fasta,
+                first_len,
+                second_len
+            )
 
-            # create partition file
-            with open(file_occupancy, "a") as f:
-                # determine number of present taxa
-                num_present  = len(og_taxa)
-                # determine number of missing taxa
-                num_missing  = len(missing_taxa)
-                # determine percent occupied
-                percent_occupancy = num_present/(num_present+num_missing)
-                if num_missing == 0:
-                    missing_taxa = ["None"]
-                entry = str(fasta)+'\t'+str(num_present)+'\t'+str(num_missing)+'\t'+str(percent_occupancy)+'\t'+';'.join(missing_taxa)+'\t'+';'.join(og_taxa)+"\n"
-                f.write(str(entry))
+            # append to occupancy file
+            self.occupancy_file_write(
+                file_occupancy,
+                og_taxa,
+                missing_taxa,
+                fasta
+            )
 
-        # join seqs of genes in value (list) and write to concat_fa   
-        with open(fasta_output, "w") as final_fasta_file: 
-            for x in concat:
-                concatenated = Seq("")
-                for s in concat[x]:
-                    concatenated += s
-                concat[x] = concatenated
-                entry = '>'+x+"\n"+concat[x]+"\n"
-                final_fasta_file.write(str(entry))             
+        # write output fasta file
+        self.fasta_file_write(
+            fasta_output,
+            concat
+        )
 
         # print log message
         print("Complete!\n")
