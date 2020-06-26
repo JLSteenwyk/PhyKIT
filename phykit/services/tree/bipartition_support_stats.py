2548a308f32b2d4fe7db959119bd81f650c4b6c3
Jacob Steenwyk <jlsteenwyk@gmail.com>
2020-06-26 13:44:52 -0700

    refactored bipartition_support_stats

--- phykit/services/tree/bipartition_support_stats.py
+++ phykit/services/tree/bipartition_support_stats.py
@@ -9,6 +9,7 @@ from Bio import Phylo
 import numpy as np
 
 from .base import Tree
+from ...helpers.stats_summary import calculate_summary_statistics_from_arr, print_summary_statistics
 
 class BipartitionSupportStats(Tree):
     def __init__(self, args) -> None:
@@ -16,25 +17,20 @@ class BipartitionSupportStats(Tree):
 
     def run(self):
         tree = self.read_tree_file()
-        bs_vals, stats = self.calculate_bipartition_support_stats(tree)
+        bs_vals = self.get_bipartition_support_vals(tree)
+
+        stats = calculate_summary_statistics_from_arr(bs_vals)  
+
         if self.verbose:
             for bs_val in bs_vals:
                 print(bs_val)
         else:
-            print(f"mean: {stats['mean']}")
-            print(f"median: {stats['median']}")
-            print(f"25th percentile: {stats['twenty_fifth']}")
-            print(f"75th percentile: {stats['seventy_fifth']}")
-            print(f"minimum: {stats['minimum']}")
-            print(f"maximum: {stats['maximum']}")
-            print(f"standard deviation: {stats['standard_deviation']}")
-            print(f"variance: {stats['variance']}")
-            
+            print_summary_statistics(stats)
 
     def process_args(self, args):
         return dict(tree_file_path=args.tree, verbose=args.verbose)
 
-    def calculate_bipartition_support_stats(self, tree):
+    def get_bipartition_support_vals(self, tree):
         # initialize list to hold bootstrap values
         bs_vals = []
 
@@ -43,16 +39,4 @@ class BipartitionSupportStats(Tree):
             # only include if a bootstrap value is present
             if terminal.confidence != None:
                 bs_vals.append(terminal.confidence)
-        
-        stats = dict(
-            mean               = stat.mean(bs_vals),
-            median             = stat.median(bs_vals),
-            twenty_fifth       = np.percentile(bs_vals, 25),
-            seventy_fifth      = np.percentile(bs_vals, 75),
-            standard_deviation = stat.stdev(bs_vals),
-            variance           = stat.variance(bs_vals),
-            minimum            = np.min(bs_vals),
-            maximum            = np.max(bs_vals)
-        )
-
-        return bs_vals, stats
+        return bs_vals
