0de851ca3e38a82434aa088e71cbc3481b493898
Jacob Steenwyk <jlsteenwyk@gmail.com>
2020-06-27 17:18:19 -0700

    refactored lb_score

--- phykit/services/tree/lb_score.py
+++ phykit/services/tree/lb_score.py
@@ -30,24 +30,31 @@ class LBScore(Tree):
     def process_args(self, args):
         return dict(tree_file_path=args.tree, verbose=args.verbose)
 
-    def calculate_lb_score(self, tree):
-        # get tree tips
-        tips = []
-        for tip in tree.get_terminals():
-            tips.append(tip.name)
-        
+    def calculate_average_distance_between_tips(
+        self,
+        tips: list,
+        tree
+    ) -> float:
         # determine pairwise combinations of tips
         combos = list(itertools.combinations(tips, 2))
 
         # determine average distance between tips
         # avg_dist is PDa
-        avg_dist = float()
+        total_dist = float()
         for combo in combos:
-            avg_dist+=tree.distance(combo[0], combo[1])
-        avg_dist = avg_dist/len(combos)
+            total_dist+=tree.distance(combo[0], combo[1])
+        
+        return total_dist/len(combos)
 
-        # calculate average distance of taxon i to all other taxa
-        # or PDi and save each result to LBi
+    def calculate_average_distance_of_taxon_to_other_taxa(
+        self,
+        tips: list,
+        tree
+    ) -> list:
+        """
+        calculate average distance of taxon to all other taxon or average PDi.
+        Save results to avg PDis list
+        """
         avg_PDis = []
         for tip in tips:
             tips_minus_i = list(set(tips) - set(tip))
@@ -57,7 +64,16 @@ class LBScore(Tree):
             PDi = sum(PDi) / len(PDi)
             avg_PDis.append(PDi)
 
-        # use PDis and avgDist to calculate LB values for each taxon
+        return avg_PDis
+
+    def calculate_lb_score_per_taxa(
+        self,
+        avg_PDis:list,
+        avg_dist:float
+    ) -> list:
+        """
+        create a list with the lb scores for each taxon
+        """
         LBis = []
         for PDi in avg_PDis:
             try:
@@ -65,5 +81,21 @@ class LBScore(Tree):
             except ZeroDivisionError:
                 print("Invalid tree. Tree should contain branch lengths")
                 return None
+        
+        return LBis
+
+    def calculate_lb_score(self, tree):
+        # get tree tips
+        tips = self.get_tip_names_from_tree(tree)
+        
+        # get average distance between tips
+        avg_dist = self.calculate_average_distance_between_tips(tips, tree)
+
+        # calculate average distance of taxon i to all other taxa
+        # or PDi and save each result to LBi
+        avg_PDis = self.calculate_average_distance_of_taxon_to_other_taxa(tips, tree)
+
+        # use PDis and avgDist to calculate LB values for each taxon
+        LBis = self.calculate_lb_score_per_taxa(avg_PDis, avg_dist)
         
         return tips, LBis
