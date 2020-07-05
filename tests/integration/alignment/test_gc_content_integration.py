2dea507bd8e4646bce9a88fb4d48c8a7db8e3cbf
Jacob Steenwyk <jlsteenwyk@gmail.com>
2020-07-05 06:25:55 -0700

    added gc content integration tests and handling of incorrect input files

--- tests/integration/alignment/test_gc_content_integration.py
+++ tests/integration/alignment/test_gc_content_integration.py
@@ -12,8 +12,8 @@ here = Path(__file__)
 @pytest.mark.integration
 class TestGCContent(object):
     @patch("builtins.print")
-    def test_gc_content(self, mocked_print):
-        expected_result = "0.2273"
+    def test_gc_content0(self, mocked_print):
+        expected_result = 0.2273
         testargs = [
             "phykit",
             "gc_content",
@@ -22,3 +22,53 @@ class TestGCContent(object):
         with patch.object(sys, "argv", testargs):
             Phykit()
         assert mocked_print.mock_calls == [call(expected_result)]
+
+    @patch("builtins.print")
+    def test_gc_content1(self, mocked_print):
+        expected_result = 0.2941
+        testargs = [
+            "phykit",
+            "gc_content",
+            f"{here.parent.parent.parent}/sample_files/test_alignment_0.fa",
+        ]
+        with patch.object(sys, "argv", testargs):
+            Phykit()
+        assert mocked_print.mock_calls == [call(expected_result)]
+
+    @patch("builtins.print")
+    def test_gc_content2(self, mocked_print):
+        expected_result = 0.25
+        testargs = [
+            "phykit",
+            "gc_content",
+            f"{here.parent.parent.parent}/sample_files/test_alignment_1.fa",
+        ]
+        with patch.object(sys, "argv", testargs):
+            Phykit()
+        assert mocked_print.mock_calls == [call(expected_result)]
+
+    @patch("builtins.print")
+    def test_gc_content2(self, mocked_print):
+        expected_result = 0.3
+        testargs = [
+            "phykit",
+            "gc_content",
+            f"{here.parent.parent.parent}/sample_files/test_alignment_2.fa",
+        ]
+        with patch.object(sys, "argv", testargs):
+            Phykit()
+        assert mocked_print.mock_calls == [call(expected_result)]
+
+    @patch("builtins.print")
+    def test_gc_content_incorrect_input_file(self, mocked_print):
+        testargs = [
+            "phykit",
+            "gc_content",
+            f"{here.parent.parent.parent}/sample_files/test_trees.txt",
+        ]
+
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            Phykit()
+
+        assert pytest_wrapped_e.type == SystemExit
+        assert pytest_wrapped_e.value.code == 2
