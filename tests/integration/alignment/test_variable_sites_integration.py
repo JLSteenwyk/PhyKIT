951f721d9eb155f9ba42c311952089f2aa43a349
Jacob Steenwyk <jlsteenwyk@gmail.com>
2020-07-05 07:04:23 -0700

    added additional integration tests for variable sites function and erroneous input file handling

--- tests/integration/alignment/test_variable_sites_integration.py
+++ tests/integration/alignment/test_variable_sites_integration.py
@@ -12,7 +12,7 @@ here = Path(__file__)
 @pytest.mark.integration
 class TestVariableSites(object):
     @patch("builtins.print")
-    def test_variable_sites(self, mocked_print):
+    def test_variable_sites0(self, mocked_print):
         expected_result = "4\t6\t66.6667"
         testargs = [
             "phykit",
@@ -22,3 +22,40 @@ class TestVariableSites(object):
         with patch.object(sys, "argv", testargs):
             Phykit()
         assert mocked_print.mock_calls == [call(expected_result)]
+
+    @patch("builtins.print")
+    def test_variable_sites1(self, mocked_print):
+        expected_result = "2\t9\t22.2222"
+        testargs = [
+            "phykit",
+            "variable_sites",
+            f"{here.parent.parent.parent}/sample_files/test_alignment_0.fa",
+        ]
+        with patch.object(sys, "argv", testargs):
+            Phykit()
+        assert mocked_print.mock_calls == [call(expected_result)]
+
+    @patch("builtins.print")
+    def test_variable_sites2(self, mocked_print):
+        expected_result = "1\t3\t33.3333"
+        testargs = [
+            "phykit",
+            "variable_sites",
+            f"{here.parent.parent.parent}/sample_files/test_alignment_1.fa",
+        ]
+        with patch.object(sys, "argv", testargs):
+            Phykit()
+        assert mocked_print.mock_calls == [call(expected_result)]
+
+    @patch("builtins.print")
+    def test_variable_sites_incorrect_input_file(self, mocked_print):
+        testargs = [
+            "phykit",
+            "rcv",
+            f"{here.parent.parent.parent}/sample_files/test_alignment_1.f",
+        ]
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            Phykit()
+
+        assert pytest_wrapped_e.type == SystemExit
+        assert pytest_wrapped_e.value.code == 2
