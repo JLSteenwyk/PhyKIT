ad9f4b64305a48b230bc71001d97b13fb4a07fd1
Jacob Steenwyk <jlsteenwyk@gmail.com>
2020-07-07 06:59:38 -0700

    created tree pruning integration tests

---
+++ tests/integration/tree/test_prune_integration.py
@@ -0,0 +1,105 @@
+import pytest
+import sys
+from math import isclose
+from mock import patch, call
+from pathlib import Path
+from textwrap import dedent
+
+from phykit.phykit import Phykit
+
+here = Path(__file__)
+
+
+@pytest.mark.integration
+class TestPruneTree(object):
+    @patch("builtins.print")
+    def test_prune(self, mocked_print):
+        testargs = [
+            "phykit",
+            "prune",
+            f"{here.parent.parent.parent}/sample_files/tree_simple.tre",
+            f"{here.parent.parent.parent}/sample_files/tree_simple_prune.txt",
+        ]
+
+        with patch.object(sys, "argv", testargs):
+            Phykit()
+
+        with open(f"{here.parent.parent}/expected/tree_simple.tre.pruned", "r") as expected_tree:
+            expected_tree_content = expected_tree.read()
+
+        with open(f"{here.parent.parent.parent}/sample_files/tree_simple.tre.pruned", "r") as out_tree:
+            out_tree_content = out_tree.read()
+
+        assert expected_tree_content == out_tree_content
+
+    @patch("builtins.print")
+    def test_prune_custom_out(self, mocked_print):
+        testargs = [
+            "phykit",
+            "prune",
+            f"{here.parent.parent.parent}/sample_files/tree_simple.tre",
+            f"{here.parent.parent.parent}/sample_files/tree_simple_prune.txt",
+            "-o",
+            f"{here.parent.parent.parent}/sample_files/tree_simple_pruned_custom_output.tre"
+        ]
+
+        with patch.object(sys, "argv", testargs):
+            Phykit()
+
+        with open(f"{here.parent.parent}/expected/tree_simple_pruned_custom_output.tre", "r") as expected_tree:
+            expected_tree_content = expected_tree.read()
+
+        with open(f"{here.parent.parent.parent}/sample_files/tree_simple_pruned_custom_output.tre", "r") as out_tree:
+            out_tree_content = out_tree.read()
+
+        assert expected_tree_content == out_tree_content
+
+    @patch("builtins.print")
+    def test_prune_wrong_path_tre(self, mocked_print):
+        testargs = [
+            "phykit",
+            "prune",
+            f"{here.parent.parent.parent}/sample_files/tree_simple.tr",
+            f"{here.parent.parent.parent}/sample_files/tree_simple_prune.txt",
+        ]
+
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            Phykit()
+
+        assert pytest_wrapped_e.type == SystemExit
+        assert pytest_wrapped_e.value.code == 2
+
+    @patch("builtins.print")
+    def test_prune_wrong_path_list(self, mocked_print):
+        testargs = [
+            "phykit",
+            "prune",
+            f"{here.parent.parent.parent}/sample_files/tree_simple.tre",
+            f"{here.parent.parent.parent}/sample_files/tree_simple_prune.tx",
+        ]
+
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            Phykit()
+
+        assert pytest_wrapped_e.type == SystemExit
+        assert pytest_wrapped_e.value.code == 2
+
+    @patch("builtins.print")
+    def test_prune_alias(self, mocked_print):
+        testargs = [
+            "phykit",
+            "prune_tree",
+            f"{here.parent.parent.parent}/sample_files/tree_simple.tre",
+            f"{here.parent.parent.parent}/sample_files/tree_simple_prune.txt",
+        ]
+
+        with patch.object(sys, "argv", testargs):
+            Phykit()
+
+        with open(f"{here.parent.parent}/expected/tree_simple.tre.pruned", "r") as expected_tree:
+            expected_tree_content = expected_tree.read()
+
+        with open(f"{here.parent.parent.parent}/sample_files/tree_simple.tre.pruned", "r") as out_tree:
+            out_tree_content = out_tree.read()
+
+        assert expected_tree_content == out_tree_content
