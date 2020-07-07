1284881b97efce4fc5d4f02a743eb64a6df2043e
Jacob Steenwyk <jlsteenwyk@gmail.com>
2020-07-07 07:46:08 -0700

    added tip labels integration tests. added additional aliases for this function.

--- tests/integration/test_tree.py
+++ tests/integration/test_tree.py
@@ -13,39 +13,6 @@ here = Path(__file__)
 @pytest.mark.integration
 class TestTree(object):
     @patch("builtins.print")
-    def test_spurious_sequence(self, mocked_print):
-        expected_result = "None"
-        testargs = [
-            "phykit",
-            "spurious_sequence",
-            f"{here.parent.parent}/sample_files/tree_simple.tre",
-        ]
-        with patch.object(sys, "argv", testargs):
-            Phykit()
-        assert mocked_print.mock_calls == [call(expected_result)]
-
-    @patch("builtins.print")
-    def test_tip_labels(self, mocked_print):
-        expected_result = "None"
-        testargs = [
-            "phykit",
-            "tip_labels",
-            f"{here.parent.parent}/sample_files/tree_simple.tre",
-        ]
-        with patch.object(sys, "argv", testargs):
-            Phykit()
-        assert mocked_print.mock_calls == [
-            call("raccoon"),
-            call("bear"),
-            call("sea_lion"),
-            call("seal"),
-            call("monkey"),
-            call("cat"),
-            call("weasel"),
-            call("dog"),
-        ]
-
-    @patch("builtins.print")
     def test_total_tree_length(self, mocked_print):
         expected_result = 277.2772
         testargs = [
