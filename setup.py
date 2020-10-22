1f5f273421d7ab1590185c031b2201b21fd2b54c
Jacob L. Steenwyk <jlsteenwyk@gmail.com>
2020-10-22 09:15:43 -0500

    testing builds with python 3.6, 3.7, 3.8

--- setup.py
+++ setup.py
@@ -10,6 +10,16 @@ about = {}
 with open(path.join(here, 'phykit', 'version.py'), encoding="utf-8") as f:
     exec(f.read(), about)
 
+CLASSIFIERS = [
+    'Operating System :: OS Independent',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Topic :: Scientific/Engineering',
+]
+
 REQUIRES = ["biopython==1.76", "scipy==1.4.1"]
 
 setup(
