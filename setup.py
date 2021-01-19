de320c02e94efccc4bc43664fa6c017dd0446424
Jacob L. Steenwyk <jlsteenwyk@gmail.com>
2021-01-19 14:17:36 -0800

    testing ci

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
+with open(path.join(here, "README.md")) as f:
     long_description = f.read()
 
 CLASSIFIERS = [
