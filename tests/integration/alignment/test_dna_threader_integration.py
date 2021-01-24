377d0ca5e2c5ce9ea52a1adb6b13c95478a813a2
Jacob L. Steenwyk <jlsteenwyk@gmail.com>
2021-01-24 08:48:28 -0800

    added additional dna_threader tests

--- tests/integration/alignment/test_dna_threader_integration.py
+++ tests/integration/alignment/test_dna_threader_integration.py
@@ -377,3 +377,139 @@ class TestDNAThreader(object):
             call(expected_result_6),
             call(expected_result_7),
         ]
+
+    @patch("builtins.print")
+    def test_dna_threader_ambig_char_false(self, mocked_print):
+        expected_result_0 = dedent(
+            """>1"""
+        )
+        expected_result_1 = dedent(
+            """AAAGGG---"""
+        )
+        expected_result_2 = dedent(
+            """>2"""  
+        )
+        expected_result_3 = dedent(
+            """AAATTTGGG"""
+        )
+        expected_result_4 = dedent(
+            """>3"""  
+        )
+        expected_result_5 = dedent(
+            """AAATTT---"""
+        )
+        expected_result_6 = dedent(
+            """>4"""  
+        )
+        expected_result_7 = dedent(
+            """AAATTT---"""
+        )
+        testargs = [
+            "phykit",
+            "p2n",
+            "-p",
+            f"{here.parent.parent.parent}/sample_files/test_alignment.prot.ambig.faa",
+            "-n",
+            f"{here.parent.parent.parent}/sample_files/test.nucl.fna",
+            "-s",
+            "F",
+        ]
+
+        with patch.object(sys, "argv", testargs):
+            Phykit()
+        assert mocked_print.mock_calls == [
+            call(expected_result_0),
+            call(expected_result_1),
+            call(expected_result_2),
+            call(expected_result_3),
+            call(expected_result_4),
+            call(expected_result_5),
+            call(expected_result_6),
+            call(expected_result_7),
+        ]
+
+    @patch("builtins.print")
+    def test_dna_threader_ambig_char_true(self, mocked_print):
+        expected_result_0 = dedent(
+            """>1"""
+        )
+        expected_result_1 = dedent(
+            """AAAGGG---"""
+        )
+        expected_result_2 = dedent(
+            """>2"""  
+        )
+        expected_result_3 = dedent(
+            """AAATTTGGG"""
+        )
+        expected_result_4 = dedent(
+            """>3"""  
+        )
+        expected_result_5 = dedent(
+            """AAATTTGGG"""
+        )
+        expected_result_6 = dedent(
+            """>4"""  
+        )
+        expected_result_7 = dedent(
+            """AAATTTGGG"""
+        )
+        testargs = [
+            "phykit",
+            "p2n",
+            "-p",
+            f"{here.parent.parent.parent}/sample_files/test_alignment.prot.ambig.faa",
+            "-n",
+            f"{here.parent.parent.parent}/sample_files/test.nucl.fna",
+            "-s",
+            "T",
+        ]
+
+        with patch.object(sys, "argv", testargs):
+            Phykit()
+        assert mocked_print.mock_calls == [
+            call(expected_result_0),
+            call(expected_result_1),
+            call(expected_result_2),
+            call(expected_result_3),
+            call(expected_result_4),
+            call(expected_result_5),
+            call(expected_result_6),
+            call(expected_result_7),
+        ]
+
+    @patch("builtins.print")
+    def test_dna_threader_aa_file_not_found(self, mocked_print):
+        testargs = [
+            "phykit",
+            "p2n",
+            "-p",
+            "not_real",
+            "-n",
+            f"{here.parent.parent.parent}/sample_files/test.nucl.fna",
+            "-s",
+            "T",
+        ]
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            Phykit()
+
+        assert pytest_wrapped_e.type == SystemExit
+        assert pytest_wrapped_e.value.code == 2
+
+    @patch("builtins.print")
+    def test_dna_threader_nucl_file_not_found(self, mocked_print):
+        testargs = [
+            "phykit",
+            "p2n",
+            "-p",
+            f"{here.parent.parent.parent}/sample_files/test_alignment.prot.ambig.faa",
+            "-n",
+            "not_real",
+            "-s",
+            "T",
+        ]
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            Phykit()
+
+        assert pytest_wrapped_e.type == SystemExit
+        assert pytest_wrapped_e.value.code == 2
