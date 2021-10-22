9721de6a3730d8f923fd5a06424e7d6f795b0d05
Jacob L. Steenwyk <jlsteenwyk@gmail.com>
2021-10-22 11:46:00 -0500

    fixed create_concat function and updated tests accordingly

--- tests/integration/alignment/test_create_concatenation_matrix_integration.py
+++ tests/integration/alignment/test_create_concatenation_matrix_integration.py
@@ -47,10 +47,10 @@ class TestCreateConcatenationMatrix(object):
             call("Complete!\n"),
         ]
 
-        with open(f"{here.parent.parent}/expected/concat_matrix.fa", "r") as expected_fa, open(
-            f"{here.parent.parent}/expected/concat_matrix.occupancy", "r"
+        with open(f"{here.parent.parent}/expected/create_concat_matrix.fa", "r") as expected_fa, open(
+            f"{here.parent.parent}/expected/create_concat_matrix.occupancy", "r"
         ) as expected_occupency, open(
-            f"{here.parent.parent}/expected/concat_matrix.partition"
+            f"{here.parent.parent}/expected/create_concat_matrix.partition"
         ) as expected_partition:
             expected_fa_content = expected_fa.read()
             expected_occupency_content = expected_occupency.read()
@@ -107,6 +107,62 @@ class TestCreateConcatenationMatrix(object):
             f"{here.parent.parent}/expected/test_alignment_concat_123.occupancy", "r"
         ) as expected_occupency, open(
             f"{here.parent.parent}/expected/test_alignment_concat_123.partition"
+        ) as expected_partition:
+            expected_fa_content = expected_fa.read()
+            expected_occupency_content = expected_occupency.read()
+            expected_partition_content = expected_partition.read()
+
+        with open(f"{prefix}.fa", "r") as out_fa, open(
+            f"{prefix}.occupancy", "r"
+        ) as out_occupency, open(f"{prefix}.partition", "r") as out_partition:
+            out_fa_content = out_fa.read()
+            out_occupency_content = out_occupency.read()
+            out_partition_content = out_partition.read()
+
+        assert expected_fa_content == out_fa_content
+        assert expected_occupency_content == out_occupency_content
+        assert expected_partition_content == out_partition_content
+
+    @patch("builtins.print")
+    def test_create_concatenation_matrix2(self, mocked_print):
+        prefix = "output/alignment_list_head"
+        expected_start_message = dedent(
+            """
+			--------------------
+			| General features |
+			--------------------
+			Total number of taxa: 20
+			Total number of alignments: 10
+			
+			
+			----------------
+			| Output files |
+			----------------
+			Partition file output: output/alignment_list_head.partition
+			Concatenated fasta output: output/alignment_list_head.fa
+			Occupancy report: output/alignment_list_head.occupancy
+			"""
+        )
+        testargs = [
+            "phykit",
+            "create_concatenation_matrix",
+            "-a",
+            f"{here.parent.parent.parent}/sample_files/alignment_list_head.txt",
+            "-p",
+            prefix,
+        ]
+        with patch.object(sys, "argv", testargs):
+            Phykit()
+
+        assert mocked_print.mock_calls == [
+            call(expected_start_message),
+            call("Complete!\n"),
+        ]
+
+        with open(f"{here.parent.parent}/expected/alignment_list_head.fa", "r") as expected_fa, open(
+            f"{here.parent.parent}/expected/alignment_list_head.occupancy", "r"
+        ) as expected_occupency, open(
+            f"{here.parent.parent}/expected/alignment_list_head.partition"
         ) as expected_partition:
             expected_fa_content = expected_fa.read()
             expected_occupency_content = expected_occupency.read()
