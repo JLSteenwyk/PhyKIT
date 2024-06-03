7ae5b47a90433e7c8a6cb94432ac3270743a195a
JLSteenwyk <jlsteenwyk@gmail.com>
2024-06-03 14:23:01 -0700

    bumping python and package versions

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ CLASSIFIERS = [
     'Programming Language :: Python',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
     'Topic :: Scientific/Engineering',
 ]
 
