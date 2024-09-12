bf474b38095db151a770f62374999b7a5a0b1d22
JLSteenwyk <jlsteenwyk@gmail.com>
2024-09-12 08:56:34 -0700

    cleaned alng function for phykit accelerate

--- phykit/services/alignment/alignment_length_no_gaps.py
+++ phykit/services/alignment/alignment_length_no_gaps.py
@@ -1,3 +1,8 @@
+from argparse import Namespace
+from typing import Dict, Tuple
+
+from Bio.Align import MultipleSeqAlignment
+
 from .base import Alignment
 
 
@@ -5,8 +10,8 @@ class AlignmentLengthNoGaps(Alignment):
     def __init__(self, args) -> None:
         super().__init__(**self.process_args(args))
 
-    def run(self):
-        alignment, alignment_format = self.get_alignment_and_format()
+    def run(self) -> None:
+        alignment, _ = self.get_alignment_and_format()
         (
             aln_len_no_gaps,
             aln_len,
@@ -14,10 +19,16 @@ class AlignmentLengthNoGaps(Alignment):
         ) = self.calculate_alignment_length_no_gaps(alignment)
         print(f"{aln_len_no_gaps}\t{aln_len}\t{round(aln_len_no_gaps_per, 4)}")
 
-    def process_args(self, args):
+    def process_args(
+        self,
+        args: Namespace,
+    ) -> Dict[str, str]:
         return dict(alignment_file_path=args.alignment)
 
-    def calculate_alignment_length_no_gaps(self, alignment):
+    def calculate_alignment_length_no_gaps(
+        self,
+        alignment: MultipleSeqAlignment,
+    ) -> Tuple[int, int, float]:
         aln_len = alignment.get_alignment_length()
         aln_len_no_gaps = self.get_sites_no_gaps_count(alignment, aln_len)
 
@@ -26,15 +37,19 @@ class AlignmentLengthNoGaps(Alignment):
 
         return aln_len_no_gaps, aln_len, aln_len_no_gaps_per
 
-    def get_sites_no_gaps_count(self, alignment, aln_len):
+    def get_sites_no_gaps_count(
+        self,
+        alignment: MultipleSeqAlignment,
+        aln_len: int,
+    ) -> int:
         """
         Count sites in the alignment with no gaps
         """
         aln_len_no_gaps = 0
-        for i in range(0, aln_len):
-            seq_at_position = ""
-            seq_at_position += alignment[:, i]
-            if "-" not in seq_at_position:
+
+        for i in range(aln_len):
+            column = alignment[:, i]
+            if "-" not in column:
                 aln_len_no_gaps += 1
 
         return aln_len_no_gaps
