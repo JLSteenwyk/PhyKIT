611474ad9a22187f4967a2a6ce2cf8d2fad85263
JLSteenwyk <jlsteenwyk@gmail.com>
2024-09-12 12:02:23 -0700

    streamlined comp bias per site function

--- phykit/services/alignment/compositional_bias_per_site.py
+++ phykit/services/alignment/compositional_bias_per_site.py
@@ -1,4 +1,9 @@
+from typing import Dict, List, Tuple, Union
+from collections import Counter
+
 from scipy.stats import chisquare, false_discovery_control
+from scipy.stats._stats_py import Power_divergenceResult
+from Bio.Align import MultipleSeqAlignment
 
 from .base import Alignment
 
@@ -7,35 +12,39 @@ class CompositionalBiasPerSite(Alignment):
     def __init__(self, args) -> None:
         super().__init__(**self.process_args(args))
 
-    def run(self):
-        alignment, _ = self.get_alignment_and_format()
-        stat_res, p_vals_corrected = self.calculate_compositional_bias_per_site(
-            alignment
-        )
-        idx = 0
-        for stat_info, pval_cor in zip(stat_res, p_vals_corrected):
-            if isinstance(pval_cor, str):
-                print(f"{idx+1}\t{round(stat_info.statistic,4)}\tnan\t{round(stat_info.pvalue,4)}")
-            else:
-                print(f"{idx+1}\t{round(stat_info.statistic,4)}\t{round(pval_cor,4)}\t{round(stat_info.pvalue,4)}")
-            idx += 1
+    def run(self) -> None:
+        alignment, _, is_protein = self.get_alignment_and_format()
 
-    def process_args(self, args):
+        stat_res, p_vals_corrected = \
+            self.calculate_compositional_bias_per_site(alignment)
+
+        for idx, (stat_info, pval_cor) in enumerate(
+            zip(stat_res, p_vals_corrected), start=1
+        ):
+            pval_cor_str = "nan" if isinstance(pval_cor, str) else round(pval_cor, 4)
+            print(f"{idx}\t{round(stat_info.statistic, 4)}\t{pval_cor_str}\t{round(stat_info.pvalue, 4)}")
+
+    def process_args(self, args) -> Dict[str, str]:
         return dict(alignment_file_path=args.alignment)
 
-    def get_number_of_occurences_per_character(self, alignment, idx: int) -> list:
-        # obtain sequence at position, remove gaps, and make
-        # all characters uppercase
-        seq_at_position = ""
-        seq_at_position += alignment[:, idx]
-        seq_at_position = seq_at_position.upper().replace("-", "")
-        num_occurences = []
-        for char in set(seq_at_position.replace("-", "")):
-            num_occurences.append(seq_at_position.count(char))
+    def get_number_of_occurrences_per_character(
+        self,
+        alignment: MultipleSeqAlignment,
+        idx: int,
+    ) -> List[int]:
+        gap_chars = self.get_gap_chars()
+        seq_at_position = alignment[:, idx].upper()
+        filtered_seq = "".join([char for char in seq_at_position if char not in gap_chars])
 
-        return num_occurences
+        return list(Counter(filtered_seq).values())
 
-    def calculate_compositional_bias_per_site(self, alignment):
+    def calculate_compositional_bias_per_site(
+        self,
+        alignment: MultipleSeqAlignment,
+    ) -> Tuple[
+        List[Power_divergenceResult],
+        List[Union[float, str]],
+    ]:
         # get aln length
         aln_len = alignment.get_alignment_length()
 
@@ -43,23 +52,24 @@ class CompositionalBiasPerSite(Alignment):
         p_vals = []
         nan_idx = []
 
-        for idx in range(0, aln_len):
-            # count occurneces of each character at site idx
-            num_occurences = self.get_number_of_occurences_per_character(
-                alignment, idx
-            )
-            
-            chisquare_res = chisquare(num_occurences)
+        for idx in range(aln_len):
+            # Count occurrences of each character at site idx
+            num_occurrences = self.get_number_of_occurrences_per_character(alignment, idx)
 
+            # Perform chi-square test on the counts
+            chisquare_res = chisquare(num_occurrences)
             stat_res.append(chisquare_res)
 
+            # Collect p-values and track NaN values
             if str(chisquare_res.pvalue) != "nan":
                 p_vals.append(chisquare_res.pvalue)
             else:
                 nan_idx.append(idx)
 
+        # Correct p-values using false discovery control
         p_vals_corrected = list(false_discovery_control(p_vals))
 
+        # Reinsert "nan" values at the appropriate positions
         for idx in reversed(nan_idx):
             p_vals_corrected.insert(idx, "nan")
 
