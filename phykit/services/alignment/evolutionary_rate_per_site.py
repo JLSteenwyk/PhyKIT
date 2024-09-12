2bb20e15898513aa4827f6c24c715bcdd43eb7d8
JLSteenwyk <jlsteenwyk@gmail.com>
2024-09-12 13:25:10 -0700

    updated and streamlined the evo rate per site function

--- phykit/services/alignment/evolutionary_rate_per_site.py
+++ phykit/services/alignment/evolutionary_rate_per_site.py
@@ -1,3 +1,8 @@
+from collections import Counter
+from typing import List, Dict
+
+from Bio.Align import MultipleSeqAlignment
+
 from .base import Alignment
 
 
@@ -6,48 +11,54 @@ class EvolutionaryRatePerSite(Alignment):
         super().__init__(**self.process_args(args))
 
     def run(self):
-        alignment, _ = self.get_alignment_and_format()
-        pic_values = self.calculate_evolutionary_rate_per_site(
-            alignment
-        )
+        alignment, _, is_protein = self.get_alignment_and_format()
+        pic_values = self.calculate_evolutionary_rate_per_site(alignment)
+
         for idx, value in enumerate(pic_values):
-            print(f"{idx+1}\t{round(value,4)}")
+            print(f"{idx + 1}\t{round(value, 4)}")
 
     def process_args(self, args):
         return dict(alignment_file_path=args.alignment)
 
-    def get_number_of_occurences_per_character(self, alignment, idx: int) -> dict:
-        # obtain sequence at position, remove gaps, and make
-        # all characters uppercase
-        seq_at_position = ""
-        seq_at_position += alignment[:, idx]
-        seq_at_position = seq_at_position.upper().replace("-", "")
-        num_occurences = {}
-        for char in set(seq_at_position.replace("-", "")):
-            num_occurences[char] = seq_at_position.count(char)
+    def remove_gap_characters(self, seq: str, gap_chars: List[str]) -> str:
+        return ''.join([char for char in seq if char not in gap_chars]).upper()
 
-        return num_occurences
+    def get_number_of_occurrences_per_character(
+        self,
+        alignment: MultipleSeqAlignment,
+        idx: int,
+        gap_chars: List[str]
+    ) -> Dict[str, int]:
+        seq_at_position = alignment[:, idx]
+        clean_seq = self.remove_gap_characters(seq_at_position, gap_chars)
 
-    def calculate_pic(self, num_occurences: dict):
-        sum_of_frequencies = 0
-        total_frequencies = sum(num_occurences.values())
-        for _, frequency in num_occurences.items():
-            sum_of_frequencies += ((frequency/total_frequencies) ** 2)
+        return Counter(clean_seq)
+
+    def calculate_pic(
+        self,
+        num_occurrences: Dict[str, int],
+    ) -> float:
+        total_frequencies = sum(num_occurrences.values())
+        sum_of_frequencies = sum(
+            (frequency / total_frequencies) ** 2
+            for frequency in num_occurrences.values()
+        )
         return 1 - sum_of_frequencies
 
-    def calculate_evolutionary_rate_per_site(self, alignment):
-        # get aln length
+    def calculate_evolutionary_rate_per_site(
+        self,
+        alignment: MultipleSeqAlignment,
+    ) -> List[float]:
         aln_len = alignment.get_alignment_length()
-
         pic_values = []
 
-        # count number of parsimony informative sites
-        for idx in range(0, aln_len, int(1)):
-            # count occurneces of each character at site idx
-            num_occurences = self.get_number_of_occurences_per_character(
-                alignment, idx
-            )
+        gap_chars = self.get_gap_chars()
 
-            pic_values.append(self.calculate_pic(num_occurences))
+        # calculate PIC for each site in the alignment
+        for idx in range(aln_len):
+            num_occurrences = self.get_number_of_occurrences_per_character(
+                alignment, idx, gap_chars
+            )
+            pic_values.append(self.calculate_pic(num_occurrences))
 
         return pic_values
