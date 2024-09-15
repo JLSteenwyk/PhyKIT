095abdfe5c98aceee04c3c80face3bb90cf60c91
JLSteenwyk <jlsteenwyk@gmail.com>
2024-09-15 07:52:13 -0700

    updated root tree function

--- phykit/services/tree/rf_distance.py
+++ phykit/services/tree/rf_distance.py
@@ -1,3 +1,7 @@
+from typing import Dict, List
+
+from Bio.Phylo import Newick
+
 from .base import Tree
 
 
@@ -9,18 +13,17 @@ class RobinsonFouldsDistance(Tree):
         tree_zero = self.read_tree_file()
         tree_one = self.read_tree1_file()
 
-        # get tree tip names
+        # get shared tree tip names
         tree_zero_tips = self.get_tip_names_from_tree(tree_zero)
         tree_one_tips = self.get_tip_names_from_tree(tree_one)
-
-        # determine shared tips, tips to prune, and prune tips
         shared_tree_tips = self.shared_tips(tree_zero_tips, tree_one_tips)
+
+        # prune to common set
         tree_zero_tips_to_prune = list(set(tree_zero_tips) - set(shared_tree_tips))
         tree_one_tips_to_prune = list(set(tree_one_tips) - set(shared_tree_tips))
         tree_zero = self.prune_tree_using_taxa_list(tree_zero, tree_zero_tips_to_prune)
         tree_one = self.prune_tree_using_taxa_list(tree_one, tree_one_tips_to_prune)
 
-        tip_for_rooting = ""
         for term in tree_zero.get_terminals():
             tip_for_rooting = term.name
             break
@@ -33,22 +36,29 @@ class RobinsonFouldsDistance(Tree):
 
         print(f"{plain_rf}\t{round(normalized_rf, 4)}")
 
-    def process_args(self, args):
-        return dict(tree_file_path=args.tree_zero, tree1_file_path=args.tree_one)
+    def process_args(self, args) -> Dict[str, str]:
+        return dict(
+            tree_file_path=args.tree_zero,
+            tree1_file_path=args.tree_one,
+        )
 
     def calculate_robinson_foulds_distance(self, tree_zero, tree_one):
         plain_rf = 0
         plain_rf = self.compare_trees(plain_rf, tree_zero, tree_one)
         plain_rf = self.compare_trees(plain_rf, tree_one, tree_zero)
-        # count the number of tips in a phylogeny
+
         tip_count = tree_zero.count_terminals()
 
-        # calculate normalized rf distance
         normalized_rf = plain_rf / (2 * (tip_count - 3))
 
         return plain_rf, normalized_rf
 
-    def compare_trees(self, plain_rf: int, tree_zero: Tree, tree_one: Tree) -> int:
+    def compare_trees(
+        self,
+        plain_rf: int,
+        tree_zero: Newick.Tree,
+        tree_one: Newick.Tree
+    ) -> int:
         # loop through tree_zero and find similar clade in tree_one
         for clade_zero in tree_zero.get_nonterminals()[1:]:
             # initialize and populate a list of tip names in tree_zero
@@ -65,11 +75,11 @@ class RobinsonFouldsDistance(Tree):
         return plain_rf
 
     def determine_if_clade_differs(
-        self, plain_rf: int, tip_names_zero: list, tip_names_one: list
+        self,
+        plain_rf: int,
+        tip_names_zero: List[str],
+        tip_names_one: List[str],
     ) -> int:
-        """
-        if clade differs, add 1 to plain_rf value
-        """
         if set(tip_names_zero) != set(tip_names_one):
             plain_rf += 1
 
