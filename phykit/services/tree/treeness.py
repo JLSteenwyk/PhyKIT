8c1bdba94828a2699a74287a4c6059695068a25b
JLSteenwyk <jlsteenwyk@gmail.com>
2024-09-15 12:01:42 -0700

    cleaned up tor and treeness functions

--- phykit/services/tree/treeness.py
+++ phykit/services/tree/treeness.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from .base import Tree
 
 
@@ -5,10 +7,10 @@ class Treeness(Tree):
     def __init__(self, args) -> None:
         super().__init__(**self.process_args(args))
 
-    def run(self):
+    def run(self) -> None:
         tree = self.read_tree_file()
         treeness = self.calculate_treeness(tree)
         print(round(treeness, 4))
 
-    def process_args(self, args):
+    def process_args(self, args) -> Dict[str, str]:
         return dict(tree_file_path=args.tree)
--- tests/unit/services/tree/test_treeness_over_rcv.py
+++ tests/unit/services/tree/test_treeness_over_rcv.py
@@ -1,6 +1,5 @@
 import pytest
 from argparse import Namespace
-from Bio import Phylo
 from math import isclose
 
 from phykit.services.tree.treeness_over_rcv import TreenessOverRCV
@@ -24,11 +23,3 @@ class TestTreenessOverRCV(object):
         t = TreenessOverRCV(args)
         t.read_tree_file()
         mock_read.assert_called_with(args.tree, "newick")
-
-    def test_calculate_treeness_over_rcv(self, args):
-        t = TreenessOverRCV(args)
-        treeness = 0.12599722400563595
-        rcv = 0.36
-        res = t.calculate_treeness_over_rcv(treeness, rcv)
-        assert isinstance(res, float)
-        assert isclose(res, 0.3499922889045443, rel_tol=0.001)
