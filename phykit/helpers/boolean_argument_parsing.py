2940be472f711deda6448fd21ce17ecc64eaafd9
Capo <capo@localhost>
2026-09-13 07:46:17 -0700

    Accept yes/no spellings in boolean CLI arguments

--- phykit/helpers/boolean_argument_parsing.py
+++ phykit/helpers/boolean_argument_parsing.py
@@ -1,10 +1,15 @@
 def str2bool(v):
+    """Parse case-insensitive boolean strings, passing bool objects through.
+
+    Accept true/t/1/yes/y as True and false/f/0/no/n as False.
+    Invalid strings raise argparse.ArgumentTypeError.
+    """
     if isinstance(v, bool):
         return v
     value = v.lower()
-    if value in ("true", "t", "1"):
+    if value in ("true", "t", "1", "yes", "y"):
         return True
-    elif value in ("false", "f", "0"):
+    elif value in ("false", "f", "0", "no", "n"):
         return False
     else:
         import argparse
--- tests/unit/helpers/test_boolean_argument_parsing.py
+++ tests/unit/helpers/test_boolean_argument_parsing.py
@@ -7,12 +7,28 @@ import pytest
 from phykit.helpers.boolean_argument_parsing import str2bool
 
 
+YES_NO_CASES = [
+    ("yes", True),
+    ("YES", True),
+    ("Yes", True),
+    ("y", True),
+    ("Y", True),
+    ("no", False),
+    ("NO", False),
+    ("No", False),
+    ("n", False),
+    ("N", False),
+]
+
+
 def test_import_and_valid_parse_do_not_import_argparse():
     code = """
 import sys
 from phykit.helpers.boolean_argument_parsing import str2bool
-assert str2bool("true") is True
-assert str2bool("false") is False
+for value in ("true", "t", "1", "yes", "YES", "Yes", "y", "Y", True):
+    assert str2bool(value) is True
+for value in ("false", "f", "0", "no", "NO", "No", "n", "N", False):
+    assert str2bool(value) is False
 assert "argparse" not in sys.modules
 """
     subprocess.run([sys.executable, "-c", code], check=True)
@@ -29,10 +45,27 @@ class LowerCountingStr(str):
         return super().lower()
 
 
-def test_str2bool_normalizes_string_once():
-    v = LowerCountingStr("false")
-    assert str2bool(v) is False
+@pytest.mark.parametrize(
+    "value, expected",
+    [("true", True), ("false", False)] + YES_NO_CASES,
+)
+def test_str2bool_normalizes_string_once(value, expected):
+    v = LowerCountingStr(value)
+    assert str2bool(v) is expected
     assert v.lower_calls == 1
+
+
+@pytest.mark.parametrize("value, expected", YES_NO_CASES)
+def test_str2bool_yes_no_spellings(value, expected):
+    assert str2bool(value) is expected
+
+
+@pytest.mark.parametrize("value, expected", YES_NO_CASES)
+def test_argparse_yes_no_spellings(value, expected):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--flag", type=str2bool)
+    args = parser.parse_args(["--flag", value])
+    assert args.flag is expected
 
 
 class TestBooleanHandling(object):
@@ -76,8 +109,10 @@ class TestBooleanHandling(object):
         v = str2bool(v)
         assert v is False
 
-    def test_str2bool_argument_type_error(self):
-        v = 'Not_valid'
+    @pytest.mark.parametrize("value", ["Not_valid", "maybe"])
+    def test_str2bool_argument_type_error(self, value):
+        v = LowerCountingStr(value)
         with pytest.raises(argparse.ArgumentTypeError) as excinfo:
-            v = str2bool(v)
-        assert "Boolean value expected." in str(excinfo.value)
+            str2bool(v)
+        assert str(excinfo.value) == "Boolean value expected."
+        assert v.lower_calls == 1
